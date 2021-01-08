914462ca22a85e8d0159edeae365e7d794b78317
Pawcio <paulanersilva@gmail.com>
2021-01-09 00:09:18 +0100

    update to failure crud

--- MarzenieLaboranta.Application/Services/FailuresService.cs
+++ MarzenieLaboranta.Application/Services/FailuresService.cs
@@ -12,15 +12,17 @@ namespace MarzenieLaboranta.Application.Services
     public class FailuresService : IFailuresService
     {
         private readonly IFailuresRepository _failuresRepository;
-       
-        public FailuresService(IFailuresRepository failuresRepository)
+        private readonly IResourcesRepository _resourcesRepository;
+
+        public FailuresService(IFailuresRepository failuresRepository, IResourcesRepository resourcesRepository)
         {
             _failuresRepository = failuresRepository;
+            _resourcesRepository = resourcesRepository;
         }
 
         public async Task<long> AddFailureReport(AddFailureCommand command)
         {
-            var failureReport = new FailureReport(command.FailureDescription, command.ReporterId, DateTime.Now, RepairStatusEnum.Waiting);
+            var failureReport = new FailureReport(command.FailureDescription, command.ResourceId, command.ReporterId, DateTime.Now, RepairStatusEnum.Waiting);
 
             return await _failuresRepository.AddFailuresReport(failureReport);
         }
@@ -45,6 +47,13 @@ namespace MarzenieLaboranta.Application.Services
             }
             failureReport.RepairmanId = command.RepairmanId;
             failureReport.RepairStatus = command.RepairStatus;
+
+            if (failureReport.RepairStatus == RepairStatusEnum.ForScrapping)
+            {
+                var resource = await _resourcesRepository.GetResource(failureReport.ResourceId);
+                resource.Scrap();
+                await _resourcesRepository.UpdateResource(resource);
+            }
 
             await _failuresRepository.UpdateFailureReport(failureReport);
         }
