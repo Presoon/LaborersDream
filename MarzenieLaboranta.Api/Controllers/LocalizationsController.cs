177fe62193ca4ac7919be1f964f276062ccc54ea
Anna Dzierżawa <annadzierzawa@gmail.com>
2021-01-10 15:13:26 +0100

    Localization Controller empty file added

---
+++ MarzenieLaboranta.Api/Controllers/LocalizationsController.cs
@@ -0,0 +1,11 @@
+﻿using MarzenieLaboranta.Api.Roles;
+using MarzenieLaboranta.Application.Commands;
+using MarzenieLaboranta.Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MarzenieLaboranta.Api.Controllers
+{
+   
+}
