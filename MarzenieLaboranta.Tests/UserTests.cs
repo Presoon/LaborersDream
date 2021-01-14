9b6ad5b1a4eba5aa59d9c45c1a5bde458a0111ae
Anna Dzierżawa <annadzierzawa@gmail.com>
2021-01-14 22:26:07 +0100

    test project added and user, localization and resources tests added

---
+++ MarzenieLaboranta.Tests/UserTests.cs
@@ -0,0 +1,25 @@
+﻿using MarzenieLaboranta.Domain.Entities;
+using MarzenieLaboranta.Domain.Enums;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MarzenieLaboranta.Tests
+{
+    public class UserTests
+    {
+        [Fact]
+        public void GivenCorrectData_WhenCreatingNewUser_ThenUserIsCreated()
+        {
+            var user = new User("Anna", "Wanna", "AniWan", "AnnWanna1!", RoleEnum.Admin);
+
+            user.Name.ShouldBe("Anna");
+            user.Surname.ShouldBe("Wanna");
+            user.Login.ShouldBe("AniWan");
+            user.Password.ShouldBe("AnnWanna1!");
+            user.Role.ShouldBe(RoleEnum.Admin);
+        }
+    }
+}
